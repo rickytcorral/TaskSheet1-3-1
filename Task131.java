80500e0ab4c11ed4ab82dde402e0255c4893e847
rickytcorral <154580337+rickytcorral@users.noreply.github.com>
2024-02-10 12:26:03 +0000

    done

--- Task131.java
+++ Task131.java
@@ -1,3 +1,53 @@
-Task131 {
+public class Meal {
+    String[] ingredients;
 
+    public Meal(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void showIngredients() {
+        System.out.print("Ingredients: ");
+        for (String ingredient : ingredients) {
+            System.out.print(ingredient + " ");
+        }
+        System.out.println();
+    }
+}
+
+class Afritada extends Meal {
+    public Afritada() {
+        super(new String[]{"Tomato Sauce","Meat"});
+    }
+}
+
+class Mechado extends Meal {
+    public Mechado() {
+        super(new String[]{"Tomato Sauce","Meat","Potatoes & Carrots","Tomato Paste"});
+    }
+}
+
+class Menudo extends Meal {
+    public Menudo() {
+        super(new String[]{"Tomato Sauce","Meat","Potatoes & Carrots","Liver Spread","Raisins","Hotdog"});
+    }
+}
+
+class Caldereta extends Meal {
+    public Caldereta() {
+        super(new String[]{"Tomato Sauce","Meat","Potatoes & Carrots","Tomato Paste","Liver Spread","Raisins","Hotdog","Cheese"});
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Afritada meal1 = new Afritada();
+        Mechado meal2 = new Mechado();
+        Menudo meal3 = new Menudo();
+        Caldereta meal4 = new Caldereta();
+
+        meal1.showIngredients();
+        meal2.showIngredients();
+        meal3.showIngredients();
+        meal4.showIngredients();
+    }
 }
